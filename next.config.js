fb422d96b0e50576156cd45d1ab4280c649d3196
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-08-10 19:51:41 -0400

    chore: update next config

--- next.config.js
+++ next.config.js
@@ -8,21 +8,18 @@ const nextConfig = {
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     // domains: ['res.cloudinary.com'],
-    formats: ['image/png'],
-  },
+    formats: ['image/png']
+  }
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
-module.exports = [
-  nextConfig,
-  withImages({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      });
-      return config;
-    },
-  }),
-];
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  }
+};
