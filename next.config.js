38792c9a1da60f33c14ae0fbc142c9f6017e2faf
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-08-10 19:30:59 -0400

    chore: update next-config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} +*/
+// next.config.js
+const withImages = require('next-images');
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  disableStaticImages: false,
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    // domains: ['res.cloudinary.com'],
+    formats: ['image/png'],
+  },
+};
 
-module.exports = nextConfig
+// eslint-disable-next-line import/no-anonymous-default-export
+module.exports = [
+  nextConfig,
+  withImages({
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
+  }),
+];
