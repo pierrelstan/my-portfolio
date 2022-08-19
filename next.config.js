36cd2cf1118759d6f0e613adfd67e26c95793cec
Stanley Pierre Louis <stanleypl75@gmail.com>
2022-08-19 14:36:18 -0400

    fix: update next config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,2 @@
-/** @type {import('next').NextConfig} +*/
-// next.config.js
-const withImages = require('next-images');
-
-const nextConfig = {
-  reactStrictMode: true,
-  disableStaticImages: false,
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    // domains: ['res.cloudinary.com'],
-    formats: ['image/png']
-  }
-};
-
-// eslint-disable-next-line import/no-anonymous-default-export
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    });
-
-    return config;
-  }
-};
+const withImages = require("next-images");
+module.exports = withImages();
