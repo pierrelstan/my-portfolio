ad8608926bde4060f4928559448061a21d56ccd5
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-03-24 20:19:29 -0400

    chore: add navlinks data

---
+++ src/lib/Navlinks.ts
@@ -0,0 +1,15 @@
+export const navLinks = [
+  { name: 'Home', path: '/' },
+  {
+    name: 'Projects',
+    path: '#projects'
+  },
+  {
+    name: 'About',
+    path: '#about'
+  },
+  {
+    name: 'Contact Us',
+    path: '#contact'
+  }
+];
