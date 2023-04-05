b50974234e8149e40452691201019523c2ec567c
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-04-05 14:30:18 -0400

    chore: update data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,43 +1,57 @@
 export const data = [
   {
-    title: 'Restaurant App',
-    image: '/storeLogo.png',
-    description: `A full-stack Restaurant app Web App Build with the MERN STACK`,
-    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
-    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    title: 'Lo Movie',
+    image: '/loMovie.png',
+    description:
+      'A mobile app for movie enthusiasts, built with  React-Native, Redux, React-Native-Navigation.',
+    demo: 'https://github.com/pierrelstan/lo-movies',
+    repo: 'https://github.com/pierrelstan/lo-movies',
+
     completed: true
   },
   {
-    title: 'Restaurant App',
-    image: '/storeLogo.png',
-    description: `A full-stack Restaurant app Web App Build with the MERN STACK`,
-    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
-    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    title: 'Echo Store',
+    image: '/echo.png',
+    description: `A Web app for digital gamers , built with the Mern Stack.`,
+    demo: 'https://echo-one.vercel.app/',
+    repo: 'https://github.com/pierrelstan/echo',
     completed: true
   },
   {
-    title: 'E-Commerce',
+    title: 'Yaya Resto',
+    image: '/yayaresto.png',
+    description: `A  Web App for foodies , built  with the Mern Stack.`,
+    demo: 'https://yayaresto.vercel.app',
+    repo: 'https://github.com/pierrelstan/yayaresto',
+
+    completed: true
+  },
+  {
+    title: 'Shopwit',
     image: '/storeLogo.png',
-    description: `A full-stack E-Commerce Web App Build with the MERN STACK`,
-    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
-    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    description: `A  Web app for shoppers, built with the Mern Stack.`,
+    repo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
+    demo: 'https://github.com/pierrelstan/shopwitapp-frontend',
     completed: true
   },
   {
     title: 'Reddit Timer',
     image: '/redditLogo.png',
     description:
-      'A Web app for professional content creators, build with Reactjs.',
+      'A Web app for professional content creators, built with Reactjs.',
+    demo: 'https://reddit-timer-stanley.netlify.app',
     repo: 'https://github.com/pierrelstan/reddit-timer-pierrelstan',
-    demo: 'reddit-timer-stanley.netlify.app',
+
     completed: true
   },
+
   {
     title: 'Bug Tracker',
     image: '/im.png',
     description: '',
-    repo: 'github',
     demo: 'Demo',
+    repo: 'github',
+
     completed: false
   }
 ];
