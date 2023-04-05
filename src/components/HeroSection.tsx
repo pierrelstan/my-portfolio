eec3dbed7d428b28c152e447941b87b28753cf0d
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-04-04 21:37:11 -0400

    style: remove section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,54 +1,53 @@
-import { Grid, Typography, Box } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import React from 'react';
-
-import Section from './common/Section';
 import Icons from './common/Icons';
 
 export default function SectionHero() {
   return (
-    <Section>
-      <Grid
-        container
-        id="home"
-        spacing={2}
+    <Grid
+      id="home"
+      sx={{
+        mt: [0, 18],
+        display: 'flex',
+        flexDirection: 'column',
+        height: {
+          lg: 'auto',
+          xs: '100vh'
+        },
+        justifyContent: 'center'
+      }}>
+      <Typography
+        variant="h1"
         sx={{
-          mt: 18
+          color: 'text.primary',
+          fontSize: {
+            lg: 140,
+            md: 130,
+            sm: 100,
+            xs: 70
+          },
+          fontWeight: 800
         }}>
-        <Grid>
-          <Typography
-            variant="h1"
-            sx={{
-              color: 'text.primary',
-              fontSize: {
-                lg: 140,
-                md: 130,
-                sm: 100,
-                xs: 70
-              },
-              fontWeight: 800
-            }}>
-            Hey!
-          </Typography>
-          <Typography
-            variant="h2"
-            gutterBottom
-            sx={{
-              mt: 2,
-              color: 'text.primary',
-              fontSize: {
-                lg: 60,
-                md: 60,
-                sm: 60,
-                xs: 20
-              }
-            }}>
-            My name is <strong>Stanley Pierre Louis</strong>, <br /> I build
-            things for the <strong>Web</strong> and <br />
-            <strong>Mobile</strong>.
-          </Typography>
-          <Icons />
-        </Grid>
-      </Grid>
-    </Section>
+        Hey!
+      </Typography>
+      <Typography
+        variant="h2"
+        gutterBottom
+        sx={{
+          mt: 2,
+          color: 'text.primary',
+          fontSize: {
+            lg: 60,
+            md: 60,
+            sm: 60,
+            xs: 20
+          }
+        }}>
+        My name is <strong>Stanley Pierre Louis</strong>, <br /> I build things
+        for the <strong>Web</strong> and <br />
+        <strong>Mobile</strong>.
+      </Typography>
+      <Icons />
+    </Grid>
   );
 }
