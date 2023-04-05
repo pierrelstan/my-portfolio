e8751ba4a8301bc373fee40a6e6616e92586ea88
Stanley Pierre-Louis <stanleypl75@gmail.com>
2023-04-04 21:51:16 -0400

    chore: wip

---
+++ src/lib/data.ts
@@ -0,0 +1,43 @@
+export const data = [
+  {
+    title: 'Restaurant App',
+    image: '/storeLogo.png',
+    description: `A full-stack Restaurant app Web App Build with the MERN STACK`,
+    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
+    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    completed: true
+  },
+  {
+    title: 'Restaurant App',
+    image: '/storeLogo.png',
+    description: `A full-stack Restaurant app Web App Build with the MERN STACK`,
+    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
+    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    completed: true
+  },
+  {
+    title: 'E-Commerce',
+    image: '/storeLogo.png',
+    description: `A full-stack E-Commerce Web App Build with the MERN STACK`,
+    demo: 'https://pierrelstan.github.io/shopwitapp-frontend/#/shopwitapp-frontend/',
+    repo: 'https://github.com/pierrelstan/shopwitapp-frontend',
+    completed: true
+  },
+  {
+    title: 'Reddit Timer',
+    image: '/redditLogo.png',
+    description:
+      'A Web app for professional content creators, build with Reactjs.',
+    repo: 'https://github.com/pierrelstan/reddit-timer-pierrelstan',
+    demo: 'reddit-timer-stanley.netlify.app',
+    completed: true
+  },
+  {
+    title: 'Bug Tracker',
+    image: '/im.png',
+    description: '',
+    repo: 'github',
+    demo: 'Demo',
+    completed: false
+  }
+];
